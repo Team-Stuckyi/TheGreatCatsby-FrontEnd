9bd4de14b498956144f5815e2d5b0c5be2c8d443
ByeongminLee <bmlee531@gmail.com>
2022-02-21 20:36:31 +0900

    [Feat] Prettier 세팅

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 140,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    proseWrap: 'never',
+    endOfLine: 'auto',
+};
