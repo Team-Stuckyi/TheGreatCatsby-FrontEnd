8ae249e88093cdb64d052b27f88b42b8fc51bdec
Seulgi <dltmfrl600@gmail.com>
2022-02-23 13:03:56 +0900

    Feat: ReviewCard 컴포넌트 수정 #7
    - ReviewList 상위 컴포넌트로부터 props를 받아오도록 변경
    - 주석 처리

--- src/components/common/ReviewCard.js
+++ src/components/common/ReviewCard.js
@@ -1,5 +1,9 @@
-import axios from 'axios';
-import React, {useState, useEffect} from 'react';
+/**
+ * @filename    : ReviewCard.js
+ * @author      : 이슬기 (https://github.com/abcabcp)
+ * @description : 상위 컴포넌트인 ReviewList에 들어갈 항목을 정의한 스타일 컴포넌트
+ */
+
 import styled, { css } from "styled-components";
 
 const ReviewContainer = styled.div`
@@ -55,60 +59,35 @@ const ReviewContents = styled.p`
     `}
 `;
 
-//상품 페이지나 기타 등등에서 ProdId를 받아와야 한다.
+/**
+ * @param ReviewObj 상위 컴포넌트인 Reviewlist에서 전달된 props
+ */
 
-const ReviewCard = ( { ProdId } ) => {
-    const [review, setReview] = useState([]);
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const response = await axios.get('http://121.160.207.51/reviews/look');
-                setReview(response.data);
-            } catch (err) {
-                console.log(err);
-                alert("연동 실패");
-            }
-        })();
-    }, []);
-
-    const result = review.item;
-    //만약 프롭스로 받은 ProdId와 서버에서 받은 ProdId가 같을 때,,,?
-    // => 여기서 setReview에 들어있는 prod_id와 비교가 가능해야 하는데 prod_id가 테이블에 없기에 비교가 힘듬.
-    /**
-     * const [prodId, setProdId] = useState("")
-     * const prodId = axios.get("~~~~~~")
-     * 
-     * if(prodID === ProdId){
-     *  리턴 값으로 띄워주는 코드
-     * }
-     */
-
+const ReviewCard = ( { ReviewObj } ) => { 
     return (
-        <div>
-            {result?.map((item, index) => {
-                return (
-                    <ReviewContainer className="review-card" container={true} key={index}>
-                        <ReviewContainer wrap={true}>
-                            <img src={item.review_photo} style={{width: "118px"}}alt="리뷰이미지" />
-                        </ReviewContainer>
-                        <ReviewContainer infoContainer={true}>
-                            <ReviewContainer info={true}>
-                                <div className="review-info-star">
-                                    {item.stars}
-                                </div>
-                                <ReviewUl>
-                                    <ReviewLi>구매인증됨</ReviewLi>
-                                    <ReviewLi>{item.write_date}</ReviewLi>
-                                </ReviewUl>
-                            </ReviewContainer>
-                            <ReviewContents name={true} className="anonymous"> {item.name.substring(0,1)}*{item.name.substring(item.name.length-1)} </ReviewContents>
-                            <ReviewContents className="text-base">{item.review_text}</ReviewContents>
-                        </ReviewContainer>
-                    </ReviewContainer>
-                )
-            })}
-        </div>
+        <ReviewContainer className="review-card" container={true}>
+            {/* 리뷰 이미지 */}
+            <ReviewContainer wrap="{true}">
+                <img src={ReviewObj.review_photo} style={{width: "118px"}}alt="리뷰이미지" />
+            </ReviewContainer>
+            <ReviewContainer infoContainer={true}>
+                <ReviewContainer info={true}>
+                    {/* 평점 */}
+                    <div className="review-info-star">
+                        {ReviewObj.stars}
+                    </div>
+                    <ReviewUl>
+                        <ReviewLi>구매인증됨</ReviewLi>
+                        {/* 작성 일자 */}
+                        <ReviewLi>{ReviewObj.write_date}</ReviewLi>
+                    </ReviewUl>
+                </ReviewContainer>
+                {/* 작성자 */}
+                <ReviewContents name="{true}" className="anonymous"> {ReviewObj.name.substring(0,1)}*{ReviewObj.name.substring(ReviewObj.name.length-1)} </ReviewContents>
+                {/* 리뷰 작성글 */}
+                <ReviewContents className="text-base">{ReviewObj.review_text}</ReviewContents>
+            </ReviewContainer>
+        </ReviewContainer>
     );
 }
 
