34fa445dac52fe68579398384494f3ea78a30331
bmlee531@gmail.com <bmlee531@gmail.com>
2022-02-23 19:20:36 +0900

    Update: 버튼 hover 추가
    - 버튼 위에 마우스를 올릴 시 밝기를 95%로 조절하는 hover값 사용
    - 기본값 밝기 95%
    - props로 'off'를 주면 hover값 사용x
    - 그외에 밝기 값을 props로 조절

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -12,6 +12,7 @@ import styled from 'styled-components';
  * @param   {string} fontColor  텍스트 색 (기본 #fff)
  * @param   {string} border 버튼 선 (기본 none)
  * @param   {string} size 버튼 높이 (기본 lg:12px, md:8px, sm:4px)
+ * @param   {string} hover 버튼 hover (기본 밝기 95%), off를 전달하여 hover 사용 없애기, hover의 밝기는 props값으로 조절 가능
  */
 
 const Button = styled.button`
@@ -27,12 +28,18 @@ const Button = styled.button`
     color: ${props => props.fontColor || 'var(--white)'};
     border: ${props => props.border || 'none'};
     padding: ${props => {
-            if (props.size === 'lg' || !props.size) return '12px';
-            else if (props.size === 'md') return '8px';
-            else if (props.size === 'sm') return '4px';
-            else return props.size;
-        }}
+        if (props.size === 'lg' || !props.size) return '12px';
+        else if (props.size === 'md') return '8px';
+        else if (props.size === 'sm') return '4px';
+        else return props.size;
+    }}
         4px;
+    &:hover {
+    filter: ${props => {
+        if (!props.hover) return 'brightness(95%)';
+        else if (props.hover === 'off') return;
+        else if (props.hover) return `brightness(${props.hover})`;
+    }}
 `;
 
 export default Button;
