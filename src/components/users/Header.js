28b8ee246dff543e8f7d518f2bb417f8e98d05e6
JeonChanMin <cksals3753@naver.com>
2022-02-24 18:57:55 +0900

    Chore: header 수정

--- src/components/users/Header.js
+++ src/components/users/Header.js
@@ -13,21 +13,16 @@ import Login from 'img/login.png';
 const HeaderContainer = styled.div`
 width: 100%;
 height: 100px;
-margin-top: 100px;
 justify-content: center;
 align-items: center;
-position: fixed;
 box-shadow: 0 12px 10px -5px var(--gray300);
-margin: auto;
-left: 0;
-top: 0;
-right: 0;
+margin: 11px auto;
 background-color: var(--white);
+margin-bottom: 50px;
 `;
 
 const TitleBar = styled.div`
 width: 1200px;
-height: 100px;
 margin: auto;
 margin-top: 5px;
 `;
@@ -38,7 +33,7 @@ float: left;
 
 const LoginBox = styled.div`
 float: right;
-margin-top: 10px;
+margin-top: 15px;
 `;
 
 const Img = styled.img`
@@ -51,20 +46,21 @@ const P = styled.p`
 color: var(--black);
 `;
 
+
 const Header = () => {
     return (
         <HeaderContainer>
             <TitleBar>
                 <LogoBox>
-                    <Link to={'/'}>
-                        <Logo Imgtype="light" Imgwidth="220px" />
-                    </Link>
+
+                    <Logo Imgtype="light" Imgwidth="220px" />
+
                 </LogoBox>
                 <LoginBox>
-                    <Link to={'/'} style={{ textDecoration: 'none' }}>
-                        <Img src={Login} />
-                        <P>Login</P>
-                    </Link>
+
+                    <Img src={Login} />
+                    <P>Login</P>
+
                 </LoginBox>
             </TitleBar>
         </HeaderContainer>
