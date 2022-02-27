191e652e8a342bf24b1cfdb56524fe19a0c5beda
Seulgi <dltmfrl600@gmail.com>
2022-02-27 22:58:48 +0900

    Feat: ListBar 컴포넌트 수정
    - 기존 코드들 재배치

--- src/components/users/ListBar.js
+++ src/components/users/ListBar.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 import {useState, useEffect} from "react";
+import ProdCard from 'components/users/ProdCard_test';
+import CardListContainer from 'components/users/ProdCardList_test';
+import Pagination from 'components/common/Pagination';
+
 
 const ListContainer =  styled.div`
     height: 40px;
@@ -37,34 +41,32 @@ const Select = styled.select`
 
 `;
 
-const ListBar = ({SortObj}) => {
-    const [asc, setAsc] = useState([]);
-    const [ready,setReady] = useState(true)
-
-    //useEffect 로 정렬하고 map 다시해보는걸로
-    useEffect(()=> {
-        setTimeout(()=>{
-            setAsc(SortObj.item);
-            setReady(false) 
-        },1000)
-    },[]);
-
-    console.log(asc);
-
-
-    return(
+const ListBar = ({TabList}) => {
+    const [limit, setLimit] = useState(20);
+    const [page, setPage] = useState(1);
+    const offset = (page -1) * limit;
+    return (
+        <>
         <ListContainer>
-            <Count>7 개의 상품</Count>
-            <SelecContainer>
-                <Label>정렬</Label>
-                <Select>
-                    <option value="star" selected >별점 높은순</option>
-                    <option value="lowPrice"> 가격 낮은순</option>
-                    <option value="highPrice">가격 높은순</option>
-                    <option value="review">후기 많은순</option>
-                </Select>
-            </SelecContainer>
-        </ListContainer>
+                <Count>{TabList.length} 개의 상품</Count>
+                <SelecContainer>
+                    <Label>정렬</Label>
+                    <Select name="sorts">
+                        <option value="0" selected>--기본--</option>
+                        <option value="1">별점 높은순</option>
+                        <option value="2">가격 낮은순</option>
+                        <option value="3">가격 높은순</option>
+                        <option value="4">후기 많은순</option>
+                    </Select>
+                </SelecContainer>
+            </ListContainer>
+            <CardListContainer>
+            {TabList?.slice(offset, offset + limit).map((content, i) => {
+                return <ProdCard key={i} content={content} />;
+            })}
+            </CardListContainer>
+            <Pagination total={TabList.length} limit={limit} page={page} setPage={setPage} />
+        </>
     )
 }
 
