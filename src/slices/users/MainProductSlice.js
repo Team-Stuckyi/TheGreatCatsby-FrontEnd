8deb892f9a2a1aff753291b5a1eef42553b0c84b
Seulgi <dltmfrl600@gmail.com>
2022-02-28 23:39:34 +0900

    Fix: MainProductSlice 수정 #33
    - 주소 수정

--- src/slices/users/MainProductSlice.js
+++ src/slices/users/MainProductSlice.js
@@ -1,14 +1,14 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { useEffect } from 'react';
 
 /** 비동기 함수 구현 */
 // payload는 이 함수를 호출할 때 전달되는 파라미터.
 
-export const getMainProdList = createAsyncThunk("/main/getMainProdList", async (payload, {rejectWithValue}) => {
+export const getMainProdList = createAsyncThunk('/main/getMainProdList', async (payload, { rejectWithValue }) => {
     let result = null;
     try {
-        result = await axios.get("http://121.160.207.51/products/main");
+        result = await axios.get('address');
     } catch (e) {
         result = rejectWithValue(e.response);
     }
@@ -19,7 +19,7 @@ export const getMainProdList = createAsyncThunk("/main/getMainProdList", async (
 export const mainProductSlice = createSlice({
     name: 'mainprod',
     initialState: {
-        rt: null,   //HTTP 상태 코드 (200, 404, 500등)
+        rt: null, //HTTP 상태 코드 (200, 404, 500등)
         rtmsg: null, //에러메시지
         item: [], //ajax 처리를 통해 수신된 데이터
         loading: false,
@@ -27,30 +27,30 @@ export const mainProductSlice = createSlice({
     },
     reducers: {},
     extraReducers: {
-        [getMainProdList.pending]: (state, {payload}) => {
-            return {...state, loading: true};
+        [getMainProdList.pending]: (state, { payload }) => {
+            return { ...state, loading: true };
         },
 
-        [getMainProdList.fulfilled]: (state, {payload}) => {
+        [getMainProdList.fulfilled]: (state, { payload }) => {
             return {
                 ...state,
                 rt: payload.status,
                 rtmsg: payload.statusText,
                 item: payload.data,
                 prods: payload.data.item,
-                loading: false
-            }
+                loading: false,
+            };
         },
-        [getMainProdList.rejected]: (state, {payload}) => {
+        [getMainProdList.rejected]: (state, { payload }) => {
             return {
                 ...state,
                 rt: payload.status,
                 rtmsg: payload.statusText,
                 item: payload.data,
-                loading: false
-            }
-        }
-    }
+                loading: false,
+            };
+        },
+    },
 });
 
 //리듀서 객체 내보내기
