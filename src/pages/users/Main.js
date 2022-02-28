f96bab8adc52c2053ec8e0cd5018343319dc3456
Seulgi <dltmfrl600@gmail.com>
2022-02-28 23:40:45 +0900

    Fix: MainPage 페이지 수정
    - prod 데이터 받은 확인 후 Tabs로 props 전달

--- src/pages/users/Main.js
+++ src/pages/users/Main.js
@@ -1,44 +1,38 @@
-import { useEffect, useState } from "react";
-import Container from "components/common/Container";
-import Header from "components/users/Header";
+import { useEffect, useState } from 'react';
+import Container from 'components/common/Container';
+import Header from 'components/users/Header';
 import Footer from 'components/users/Footer';
-import {getMainProdList} from "slices/users/MainProductSlice";
-import { useSelector, useDispatch} from "react-redux";
-import Tabs from "components/users/Tabs_test";
+import { getMainProdList } from 'slices/users/MainProductSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import Tabs from 'components/users/Tabs';
 
 const Main = () => {
     useEffect(() => console.clear(), []);
     const [prod, setProd] = useState();
-    const {rt, rtmsg, item, loading, prods} = useSelector((state) => state.mainprod);
+    const { rt, rtmsg, item, loading, prods } = useSelector(state => state.mainprod);
     const dispatch = useDispatch();
-    
+    console.log(item);
     useEffect(() => {
-        if(rt ===200){
-            setProd(prods)
+        if (rt === 200) {
+            setProd(prods);
         }
     }, [rt]);
 
-    if(loading) {
-        return (<div>Loading ...</div>)
+    if (loading) {
+        return <div>Loading ...</div>;
     }
 
-    if(rt !== 200){
-        return (
-            dispatch(getMainProdList())
-        );
+    if (rt !== 200) {
+        return dispatch(getMainProdList());
     }
-
 
     return (
         <>
-        <Header></Header>
-        <Container>
-        {prod? 
-        <Tabs Prod={prod}/>: <div>환영합니다.</div>}
-        </Container>
-        <Footer></Footer>
+            <Header></Header>
+            <Container>{prod ? <Tabs Prod={prod} /> : <div>환영합니다.</div>}</Container>
+            <Footer></Footer>
         </>
-    )
+    );
 };
 
 export default Main;
