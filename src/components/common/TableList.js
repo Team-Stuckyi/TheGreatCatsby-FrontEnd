02a2287ed3dc604d4efc911148666ebb46897337
HeeJae <heejj1206@naver.com>
2022-03-02 23:08:20 +0900

    Fix: onClick 추가, 주석 추가

--- src/components/common/TableList.js
+++ src/components/common/TableList.js
@@ -13,17 +13,29 @@ const Table = styled.table`
     border: 1px solid black;
     width: 100%;
     border-collapse: collapse;
+    margin-top: 50px;
 `;
 
 const Input = styled.input`
     text-align: center;
+    width: 90%;
     border: none !important;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    
+    &.checkBox {
+    zoom: 1.5;
+    vertical-align: middle;
+    }
 `;
 
 const TableHeader = styled.th`
     border: 1px solid black;
     padding: 10px;
     background-color: #e1e1e1;
+    font-size: 1rem;
+    line-height: 1rem;
+    font-weight: bold;: ;
 `;
 
 const TableData = styled.td`
@@ -31,7 +43,20 @@ const TableData = styled.td`
     padding: 10px;
 `;
 
-const TableList = ({ columns, data, isModifiable, isRemovable }) => {
+
+/*
+* @param   {string} columns             th에 들어갈 항목의 배열 (수정, 삭제 버튼이 들어갈 관리 th 는 고정해두었으므로 자동 생성)
+* @param   {string} data                테이블 td 에 들어갈 데이터
+* @param   {boolean} isModifiable       true 일 때 수정 버튼 생성
+* @param   {boolean} isRemovable        true 일 때 삭제 버튼 생성
+* @param   {string} removeButtonText    삭제 버튼에 들어갈 text (예시: 탈퇴)
+* @param   {function} onModifyButtonClick 수정 버튼의 클릭이벤트
+* @param   {function} onRemoveButtonClick 삭제 버튼의 클릭이벤트
+*/
+
+
+const TableList = ({ columns, data, isModifiable, isRemovable, removeButtonText, onModifyButtonClick, onRemoveButtonClick }) => {
+
     return (
         <div>
             <Table>
@@ -49,23 +74,23 @@ const TableList = ({ columns, data, isModifiable, isRemovable }) => {
                                     <TableData>{cur[key]}</TableData>
                                 ) : typeof cur[key] === 'boolean' ? (
                                     <TableData>
-                                        <Input type="checkbox" checked={cur[key]} />
+                                        <Input className="checkBox" type="checkbox" checked={cur[key]} />
                                     </TableData>
                                 ) : (
                                     <TableData>
-                                        <Input value={cur[key]} />
+                                        <Input type="text" value={cur[key]} />
                                     </TableData>
                                 ),
                             )}
                             {isModifiable ? (
                                 <TableData>
-                                    <Button bgColor={'var(--blue300)'}>수정</Button>
+                                    <Button size='8px' bgColor={'var(--blue300)'} onClick={onModifyButtonClick}>수정</Button>
                                 </TableData>
                             ) : null}
 
                             {isRemovable ? (
                                 <TableData>
-                                    <Button bgColor={'var(--primary)'}>삭제</Button>
+                                    <Button size='8px' bgColor={'var(--primary)'} onClick={onRemoveButtonClick}>{removeButtonText}</Button>
                                 </TableData>
                             ) : null}
                         </tr>
