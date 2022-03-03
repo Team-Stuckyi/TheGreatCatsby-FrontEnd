f6845417c385738acdcfe4bd238b9d09da00e829
Xeros <xeros.log@gmail.com>
2022-03-04 02:40:45 +0900

    Fix: Search.js
    - Form을 사용하는 방식으로 Search 구조 변경
    - 누락되었던 디자인 적용

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -3,16 +3,18 @@
  * @author      : 김우영 (https://github.com/0x000613)
  * @description : 검색 컴포넌트
  */
-
 import Button from 'components/common/Button';
 import Input from 'components/common/Input';
 
 const Search = ({
     categoryName = '전체회원',
     categoryCount = 0,
+    unit = '명',
     selectBoxItems = ['번호', '아이디', '내용'],
     inputBoxPlaceholder = '내용을 입력하세요',
-    searchBtnText = '검색',
+    onSubmit,
+    onChange,
+    onQueryChange,
 }) => {
     // optionList 컴포넌트 렌더링 함수
     const optionList = selectBoxItems.map((v, i) => (
@@ -22,9 +24,9 @@ const Search = ({
     ));
 
     return (
-        <div>
-            {/* countBox 렌더링 */}
-            <div style={{ maxWidth: '200px' }}>
+        <form onSubmit={onSubmit}>
+            {/** countBox 렌더링 **/}
+            <div style={{ maxWidth: '150px', maxHeight: '40px', display: 'flex', marginBottom: '5px' }}>
                 <span
                     style={{
                         width: '50%',
@@ -32,6 +34,9 @@ const Search = ({
                         borderRadius: '5px 0px 0px 5px',
                         padding: '8px',
                         backgroundColor: 'var(--blue100)',
+                        fontWeight: 'bold',
+                        fontSize: '14px',
+                        textAlign: 'center',
                     }}
                 >
                     {categoryName}
@@ -42,24 +47,47 @@ const Search = ({
                         float: 'right',
                         borderRadius: '0px 5px 5px 0px',
                         padding: '8px',
+                        paddingRight: '12px',
                         backgroundColor: 'var(--gray100)',
-                        marginBottom: '10px',
+                        fontWeight: 'bold',
+                        textAlign: 'right',
+                        fontSize: '14px',
                     }}
                 >
                     {categoryCount}
+                    {unit}
                 </span>
             </div>
             {/* SelectBox 렌더링 */}
-            <select style={{ width: '150px', height: '38px', backgroundColor: 'var(--gray200)' }}>{optionList}</select>
 
+            <select
+                style={{
+                    width: '130px',
+                    height: '42px',
+                    backgroundColor: 'var(--gray200)',
+                    borderRadius: '6px',
+                    fontSize: '14px',
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                }}
+                onChange={onChange}
+            >
+                {optionList}
+            </select>
             {/* InputBox 렌더링 */}
-            <Input InpWidth="180px" placeholder="내용을 입력하세요"></Input>
+            <Input
+                Inptype="full"
+                InpWidth="250px"
+                borderColor="var(--black)"
+                placeholder={inputBoxPlaceholder}
+                onChange={onQueryChange}
+            ></Input>
 
-            {/* Button 렌더링 */}
-            <Button width="100px" bgColor="var(--blue300)">
+            {/* Button 렌더링 **/}
+            <Button type="submit" width="100px" height="38px" bgColor="var(--blue300)">
                 검색
             </Button>
-        </div>
+        </form>
     );
 };
 
