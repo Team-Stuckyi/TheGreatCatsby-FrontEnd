4c4507004509b4f2a8c8b92e8cba113b6c47e4ab
HeeJae <heejj1206@naver.com>
2022-03-06 04:38:29 +0900

    Feat: Login 페이지, 슬라이스

--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
 import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
 import { createLogger } from 'redux-logger';
 import { mainProductSlice } from 'slices/users/MainProductSlice';
+import productsReducer from 'slices/users/products';
+import { loginSlice } from "slices/users/LoginSlice";
 
 const logger = createLogger();
 
 const store = configureStore({
     reducer: {
         mainprod: mainProductSlice.reducer,
+        products: productsReducer,
+        login: loginSlice.reducer
     },
     middleware: [...getDefaultMiddleware({serializableCheck: false,}), logger],
     devTools: true
