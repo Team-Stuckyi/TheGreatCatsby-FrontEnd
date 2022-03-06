6f96cd41109f928ded6ca636452d0b6d2b0ab714
Byeongmin Lee <71276006+ByeongminLee@users.noreply.github.com>
2022-03-06 17:47:48 +0900

    Fix: 버튼이벤트 수정
    - z-index로 생기는 버튼 클릭이 안되는 문제 해결
    - props로 click이벤트 받을 수 있게 수정
    - BgBlack에 left값을 추가하면 화면 전체를 채우도록 수정

--- src/components/common/Alert.js
+++ src/components/common/Alert.js
@@ -3,52 +3,63 @@ import styled from 'styled-components';
 import Button from 'components/common/Button';
 
 const AlertDiv = styled.div`
-position: fixed;
-z-index: 2000;
-top: 30%;
-left: calc(50% - 200px);
+    position: fixed;
+    z-index: 2000;
+    top: 30%;
+    left: calc(50% - 200px);
 `;
 
 const AlertContainer = styled.div`
-position: relative;
-width: 400px;
-height: 200px;
-background-color: var(--white);
-border-radius: 5px;
-box-shadow: 5px 5px 15px var(--gray400);
+    z-index: 2000;
+    position: relative;
+    width: 400px;
+    height: 200px;
+    background-color: var(--white);
+    border-radius: 5px;
+    box-shadow: 5px 5px 15px var(--gray400);
 `;
 
 const AlertText = styled.div`
-width: 300px;
-height: 80px;
-position: absolute;
-top: 75px;
-left: 50px;
-text-align: center;
+    width: 300px;
+    height: 80px;
+    position: absolute;
+    top: 75px;
+    left: 50px;
+    text-align: center;
 `;
 
 const BgBlack = styled.div`
-position: fixed;
-top: 0;
-z-index: 1500;
-width: 100%;
-height: 100%;
-background-color: var(--gray600);
-opacity: 0.1;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1500;
+    width: 100%;
+    height: 100%;
+    background-color: var(--gray600);
+    opacity: 0.1;
 `;
 
-const Alert = ({ text }) => {
+const ButtonBox = styled.div`
+    position: absolute;
+    top: 150px;
+    left: 50px;
+`;
+
+const Alert = ({ text, onClick }) => {
     return (
         <AlertDiv>
-            <AlertContainer>
-                <AlertText >
-                    {text}
-                </AlertText>
-                <Button width='300px'>확인</Button>
-            </AlertContainer>
             <BgBlack />
+            <AlertContainer>
+                <AlertText>{text}</AlertText>
+                <ButtonBox>
+                    <Button size="md" width="300px" onClick={onClick}>
+                        확인
+                    </Button>
+                </ButtonBox>
+            </AlertContainer>
         </AlertDiv>
-    )
+    );
 };
 
-export default Alert
+export default Alert;
+
