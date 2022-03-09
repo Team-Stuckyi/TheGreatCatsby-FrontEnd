19de24c5a45191769bec920dab5e0f3fe61b0309
bmlee531@gmail.com <bmlee531@gmail.com>
2022-03-09 17:58:42 +0900

    Update: 범용적으로 사용할 수 있도록 업데이트
    - 버튼을 옵션으로 1개 or 2개 사용 가능하게 변경

--- src/components/common/Alert.js
+++ src/components/common/Alert.js
@@ -1,54 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Button from 'components/common/Button';
 
 const AlertDiv = styled.div`
-position: fixed;
-z-index: 2000;
-top: 30%;
-left: calc(50% - 200px);
+    position: fixed;
+    z-index: 2000;
+    top: 30%;
+    left: calc(50% - 200px);
 `;
 
 const AlertContainer = styled.div`
-position: relative;
-width: 400px;
-height: 200px;
-background-color: var(--white);
-border-radius: 5px;
-box-shadow: 5px 5px 15px var(--gray400);
+    z-index: 2000;
+    position: relative;
+    width: 400px;
+    height: 200px;
+    background-color: var(--white);
+    border-radius: 5px;
+    box-shadow: 5px 5px 15px var(--gray400);
 `;
 
 const AlertText = styled.div`
-width: 300px;
-height: 80px;
-position: absolute;
-top: 75px;
-left: 50px;
-text-align: center;
+    width: 300px;
+    height: 80px;
+    position: absolute;
+    top: 75px;
+    left: 50px;
+    text-align: center;
 `;
 
 const BgBlack = styled.div`
-position: fixed;
-top: 0;
-z-index: 1500;
-width: 100%;
-height: 100%;
-background-color: var(--gray600);
-opacity: 0.1;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1500;
+    width: 100%;
+    height: 100%;
+    background-color: var(--gray600);
+    opacity: 0.1;
 `;
 
-const Alert = ({ text }) => {
+const ButtonBox = styled.div`
+    position: absolute;
+    top: 150px;
+    left: 50px;
+`;
+
+const Alert = ({ text, onClickConfirm, onClickCancel }) => {
     return (
         <AlertDiv>
-            <AlertContainer>
-                <AlertText >
-                    {text}
-                </AlertText>
-                <Button width='300px'>확인</Button>
-            </AlertContainer>
             <BgBlack />
+            <AlertContainer>
+                <AlertText>{text}</AlertText>
+                <ButtonBox>
+                    {!onClickCancel ? (
+                        <Button size="md" width="300px" bgColor="var(--blue300)" onClick={onClickConfirm}>
+                            확인
+                        </Button>
+                    ) : (
+                        <>
+                            <Button size="md" width="150px" bgColor="var(--blue300)" onClick={onClickConfirm}>
+                                확인
+                            </Button>
+                            <Button size="md" width="150px" onClick={onClickCancel}>
+                                취소
+                            </Button>
+                        </>
+                    )}
+                </ButtonBox>
+            </AlertContainer>
         </AlertDiv>
-    )
+    );
 };
 
-export default Alert
+export default Alert;
