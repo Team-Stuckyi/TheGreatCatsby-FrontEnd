cebdac8e288626f41cecdb7308b68fde9de5fcce
JeonChanMin <cksals3753@naver.com>
2022-03-16 01:49:16 +0900

    Update: radio버튼 활성 비활성 기능 추가

--- src/components/users/PayMent.js
+++ src/components/users/PayMent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Toss from 'img/toss-logo.png';
 import Kakao from 'img/kakaopay-logo.png';
@@ -9,6 +9,7 @@ height: 300px;
 `;
 const Container = styled.div`
 width: 1200px;
+margin-left: 4px;
 `;
 const TextBox = styled.div`
 width: 1100px;
@@ -27,7 +28,17 @@ margin-bottom: 30px;
     }
 `;
 
-const PayMent = () => {
+
+
+const PayMent = ({ marvel, setMarvel }) => {
+
+    // radio버튼 활성 & 비활성
+
+    const handleClickRadiobutton = e => {
+        var check = e.target.value;
+        setMarvel(check)
+    }
+    console.log(marvel);
     return (
         <>
             <Wrapper>
@@ -35,14 +46,18 @@ const PayMent = () => {
                     <TextBox>
                         <p>결제방법</p>
                     </TextBox>
-                    <RadioBox>
-                        <input type="radio" />
-                        <img src={Toss} width="80px" />
-                    </RadioBox>
-                    <RadioBox>
-                        <input type="radio" />
-                        <img src={Kakao} width="80px" />
-                    </RadioBox>
+                    <form>
+                        <RadioBox>
+                            <input type="radio" value="Toss" checked={marvel === 'Toss'} onChange={handleClickRadiobutton}
+                            />
+                            <img src={Toss} width="80px" alt="토스" />
+                        </RadioBox>
+                        <RadioBox>
+                            <input type="radio" value="Kakao" checked={marvel === 'Kakao'} onChange={handleClickRadiobutton}
+                            />
+                            <img src={Kakao} width="80px" alt="카카오" />
+                        </RadioBox>
+                    </form>
                 </Container>
             </Wrapper>
         </>
