cfac93017dc96533d5085063354d3eed4f3977f0
JeonChanMin <cksals3753@naver.com>
2022-03-16 01:57:09 +0900

    Update: 우편번호api기능, input에 value값 추가 #39

--- src/components/users/NewAdress.js
+++ src/components/users/NewAdress.js
@@ -3,6 +3,13 @@ import styled from 'styled-components';
 import Input from 'components/common/Input.js';
 import Button from 'components/common/Button.js';
 
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
+import { putAdressMember } from 'slices/users/NewMemberSlice.js';
+import { getAdressMember } from 'slices/users/NewMemberSlice.js';
+
+import DaumPostcode from 'react-daum-postcode';
+
 const Wrapper = styled.div`
 width: 100%;
 `;
@@ -51,13 +58,17 @@ width: 345px;
 float: left;
 position: relative;
 margin-bottom: 10px;
+height: 70px;
 `;
 // Button div
 const ButtonBox = styled.div`
 width: 70px;
 float: right;
 margin-right: 5px;
-margin-top: -4px;
+margin-top: -40px;
+    >Button {
+        height: 39px;
+    }
 `;
 // 밑 2개 Input div
 const InputsubBox = styled.div`
@@ -65,6 +76,7 @@ width: 370px;
 margin-top: 10px;
 margin-left: 180px;
 `;
+
 // 배송 요청사항 div
 const AdressTextsub = styled.div`
 width: 1100px;
@@ -92,7 +104,7 @@ float: left;
 `;
 // select 박스
 const Select = styled.select`
-width: 378px;
+width: 364px;
 height: 38px;
 line-height: 38px;
 text-indent: 5px;
@@ -100,7 +112,96 @@ color: var(--gray600);
 margin-top: -20px;
 font-family: InfinitySansR-Regular;
 `;
-const NewAdress = () => {
+// 우편번호 저장 div
+const AddressBox = styled.div`
+width: 260px;
+height: 70px;
+border: 1px solid var(--black);
+// 우편번호 text
+    >p {
+        font-size: 14px;
+        padding: 10px;
+        color: var(--gray500);
+    }
+`;
+const NewAdress = ({ onaddress, setOnAddress, name, setName, tel, setTel, addr1, setAddr1, addr2, setAddr2 }) => {
+
+    let { addrId } = useParams();
+    const { rt, rtmsg, item, loading } = useSelector(state => state.AdressMember);
+    const [newAddress, setNewAdress] = React.useState([]);
+    const [addressInfo, setAddressInfo] = React.useState();
+    const dispatch = useDispatch();
+
+    const onChangeAddress = (event) => {
+        const {
+            target: { name, value },
+        } = event;
+        if (name === 'rcname') {
+            setName(value);
+        } else if (name === 'rctel') {
+            setTel(value);
+        } else if (name === 'rcaddr1') {
+            setAddr1(value);
+        } else if (name === 'rcaddr2') {
+            setAddr2(value);
+        }
+    };
+
+    React.useEffect(() => {
+        dispatch(putAdressMember(addrId));
+    }, [addrId]);
+
+    React.useEffect(() => {
+        dispatch(getAdressMember(addrId));
+    }, [addrId]);
+
+    React.useEffect(() => {
+        setNewAdress(item);
+    }, [item]);
+
+    /** 우편번호 */
+    const [addressDetail, setAddressDetail] = React.useState(''); // 상세주소
+
+    const [isOpenPost, setIsOpenPost] = React.useState(false); // 팝업창
+    // 팝업창 Open
+    const onChangeOpenPost = () => {
+        setIsOpenPost(!isOpenPost);
+    };
+    // 우편번호 상태값 저장
+    const onCompletePost = (data) => {
+        let extraAddr = '';
+
+        if (data.addressType === 'R') {
+            if (data.bname !== '') {
+                extraAddr += data.bname;
+            }
+            if (data.buildingName !== '') {
+                extraAddr += extraAddr !== '' ? `, ${data.buildingName}` : data.buildingName;
+            }
+            if (extraAddr !== '') {
+                setOnAddress(data.address + ` (${extraAddr})` + `(${data.zonecode})`);
+            }
+        }
+        setAddressDetail(data.onaddress);
+        setIsOpenPost(false);
+        console.log(onaddress);
+        console.log(data.zonecode);
+    };
+
+    React.useEffect(() => {
+        console.log(onaddress);
+    }, [onaddress]);
+    // 팝업창 CSS
+    const postCodeStyle = {
+        display: "block",
+        position: "absolute",
+        top: "10%",
+        width: "600px",
+        height: "600px",
+        padding: "7px",
+        fontFamily: 'InfinitySansR-Regular',
+        border: '1px solid black'
+    };
     return (
         <>
             <Wrapper>
@@ -110,29 +211,29 @@ const NewAdress = () => {
                             <TextBox>
                                 <PayText>받는 사람</PayText>
                                 <InputBox>
-                                    <Input Inptype={'full'} Radius={'0px'} borderColor={'black'} placeholder={'받으시는 분의 성함을 입력하세요.'} />
+                                    <Input Inptype={'full'} Radius={'0px'} borderColor={'black'} placeholder={'받으시는 분의 성함을 입력하세요.'} onChange={onChangeAddress} value={name} name='rcname' />
                                 </InputBox>
                             </TextBox>
                             <TextBox>
                                 <PayText>휴대 전화</PayText>
                                 <InputBox>
-                                    <Input Inptype={'full'} Radius={'0px'} borderColor={'black'} placeholder={'휴대전화 번호를 입력하세요.'} />
+                                    <Input Inptype={'full'} Radius={'0px'} borderColor={'black'} placeholder={'휴대전화 번호를 입력하세요.'} onChange={onChangeAddress} value={tel} name='rctel' />
                                 </InputBox>
                             </TextBox>
                             <TextBox>
                                 <PayText>배송 주소</PayText>
                                 <InputButtonBox>
-                                    <Input Inptype={'full'} Radius={'0px'} InpWidth='265px' borderColor={'black'} placeholder={'우편번호를 입력하세요.'} />
+                                    <AddressBox>
+                                        <p onChange={onChangeAddress} value={addr1} name='rcaddr1' >{onaddress}</p>
+                                    </AddressBox>
                                     <ButtonBox>
-                                        <Button size={'lg'} width={'95px'}>우편번호</Button>
+                                        <Button size={'lg'} width={'95px'} onClick={onChangeOpenPost} >우편번호</Button>
                                     </ButtonBox>
                                 </InputButtonBox>
                                 <InputsubBox>
-                                    <Input Inptype={'full'} Radius={'0px'} borderColor={'black'} placeholder={'주소를 입력하세요.'} />
+                                    <Input Inptype={'full'} Radius={'0px'} borderColor={'black'} placeholder={'상세주소를 입력하세요.'} onChange={onChangeAddress} value={addr2} name='rcaddr2' />
                                 </InputsubBox>
-                                <InputsubBox>
-                                    <Input Inptype={'full'} Radius={'0px'} borderColor={'black'} placeholder={'상세주소를 입력하세요.'} />
-                                </InputsubBox>
+
                             </TextBox>
                         </AdressText>
                         <AdressTextsub>
@@ -141,13 +242,18 @@ const NewAdress = () => {
                             </AdressInfo>
                             <AdressRequest>
                                 <Select>
-                                    <option value="0" selected>배송 요청 사항을 선택하세요.</option>
+                                    <option value="0">배송 요청 사항을 선택하세요.</option>
                                     <option value="1">부재 시 연락 부탁드려요.</option>
                                     <option value="1">문 앞으로 배송 부탁드려요.</option>
                                     <option value="1">배송 전 연락 부탁드려요.</option>
                                 </Select>
                             </AdressRequest>
                         </AdressTextsub>
+                        <div>
+                            {isOpenPost ? (
+                                <DaumPostcode style={postCodeStyle} autoClose onComplete={onCompletePost} />
+                            ) : null}
+                        </div>
                     </Adress>
                 </Container>
             </Wrapper>
