562084bb3e3ece2f0aa8a94d9dc1f94e7b222cef
Seulgi <dltmfrl600@gmail.com>
2022-03-25 14:50:32 +0900

    Fix: reducer 추가
    - 사용자 페이지, 관리자 페이지 각 사용하는 reducer 추가

--- src/store.js
+++ src/store.js
@@ -1,34 +1,52 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
+//사용자 페이지
+import { appSlice } from 'slices/users/AppSlice';
+import { userLoginSlice } from 'slices/users/LoginSlice';
+import { joinMemberSlice } from 'slices/users/JoinSlice';
 import { mainProductSlice } from 'slices/users/MainProductSlice';
+import { GetOrderSlice } from './slices/users/GetOrderSlice.js';
+import { newAdressSlice } from './slices/users/NewMemberSlice.js';
+import { RecentAdressSlice } from './slices/users/RecentMemberSlice.js';
+import { ShowOrderSlice } from './slices/users/ShowOrderSlice.js';
+import { reviewListSlice } from 'slices/users/ReviewListSlice';
 import { reviewProdInfoSlice } from 'slices/users/ReviewProdInfoSlice';
-import { userLoginSlice } from "slices/users/LoginSlice";
-import { appSlice } from "slices/users/AppSlice";
-import { adminUserSlice } from 'slices/admin/AdminUserSlice';
-// import productsReducer from 'slices/users/products';
-import { adminLoginSlice } from 'slices/admin/LoginSlice';
+import { ReviewWriteSlice } from 'slices/users/ReviewWriteSlice';
+//관리자 페이지
 import { adminAppSlice } from 'slices/admin/appSlice';
-import { manageOrderSlice } from 'slices/admin/ManageOrderSlice';
+import { adminLoginSlice } from 'slices/admin/LoginSlice';
+import { adminUserSlice } from 'slices/admin/AdminUserSlice';
+import { memberSlice } from 'slices/admin/MemberSlice';
 import { manageProdSlice } from 'slices/admin/ManageProdSlice';
 import { addProdSlice } from 'slices/admin/AddProdSlice';
-import { joinMemberSlice } from 'slices/users/JoinSlice';
+import { manageOrderSlice } from 'slices/admin/ManageOrderSlice';
+import { adminReviewListSlice } from 'slices/admin/AdminReviewListSlice';
 
 const logger = createLogger();
 
 const store = configureStore({
     reducer: {
-        reviewProdInfo: reviewProdInfoSlice.reducer,
-        mainprod: mainProductSlice.reducer,
-        userLogin: userLoginSlice.reducer,
+        //사용자 페이지
         appLogin: appSlice.reducer,
-        adminuser: adminUserSlice.reducer,
-        // products: productsReducer,
+        userLogin: userLoginSlice.reducer,
+        joinMember: joinMemberSlice.reducer,
+        mainprod: mainProductSlice.reducer,
+        getOrder: GetOrderSlice.reducer,
+        showOrder: ShowOrderSlice.reducer,
+        AdressMember: newAdressSlice.reducer,
+        recentMember: RecentAdressSlice.reducer,
+        reviewDataList: reviewListSlice.reducer,
+        reviewProdInfo: reviewProdInfoSlice.reducer,
+        ReviewWrite: ReviewWriteSlice.reducer,
+        //관리자 페이지
         adminLogin: adminLoginSlice.reducer,
         adminAppLogin: adminAppSlice.reducer,
-        manageOrder: manageOrderSlice.reducer,
+        adminuser: adminUserSlice.reducer,
+        member: memberSlice.reducer,
         manageProd: manageProdSlice.reducer,
         addProd: addProdSlice.reducer,
-        joinMember: joinMemberSlice.reducer
+        manageOrder: manageOrderSlice.reducer,
+        adminReviewList: adminReviewListSlice.reducer,
     },
     middleware: [...getDefaultMiddleware({ serializableCheck: false }), logger],
     devTools: true,
