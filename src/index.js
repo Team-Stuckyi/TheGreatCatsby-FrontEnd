4a55b1cb17444357e6e9e46231f882b1b408b1d3
bmlee531@gmail.com <bmlee531@gmail.com>
2022-03-25 15:26:17 +0900

    Update: 라우터 처리

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
+import { BrowserRouter } from 'react-router-dom';
 
-import { Provider } from "react-redux";
-import store from "store";
-
+import { Provider } from 'react-redux';
+import store from 'store';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-        <App />
-        </Provider>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root'),
 );
