bcaad626a37703f72cb163948947c4c31f9fa47c
Seulgi <84028062+abcabcp@users.noreply.github.com>
2022-03-29 09:37:05 +0900

    Feat: 로딩 컴포넌트 추가
    - pending 상태일 때 Loading 컴포넌트 출력

--- src/pages/users/Main.js
+++ src/pages/users/Main.js
@@ -1,27 +1,31 @@
 import { useEffect, useState } from 'react';
-import Container from 'components/common/Container';
-import Header from 'components/users/Header';
-import Footer from 'components/users/Footer';
 import { getMainProdList } from 'slices/users/MainProductSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import Container from 'components/common/Container';
+import Loading from 'components/common/Loading';
+import Header from 'components/users/Header';
+import Footer from 'components/users/Footer';
 import Tabs from 'components/users/Tabs';
 
 const Main = () => {
     useEffect(() => console.clear(), []);
     const [prod, setProd] = useState();
-    const { rt, rtmsg, item, loading, prods } = useSelector(state => state.mainprod);
+    const { rt, rtmsg, loading, prods } = useSelector(state => state.mainprod);
     const dispatch = useDispatch();
-    console.log(item);
+
+    //rt가 200이라면 prod 상태변수의 값을 서버에서 연동받은 값으로 변경한다.
     useEffect(() => {
         if (rt === 200) {
             setProd(prods);
         }
     }, [rt]);
 
+    //아직 pending 상태라면 Loding 컴포넌트를 출력한다.
     if (loading) {
-        return <div>Loading ...</div>;
+        return <Loading />;
     }
 
+    //rt가 200이 아니라면 getMainProdList를 dispatch
     if (rt !== 200) {
         return dispatch(getMainProdList());
     }
@@ -29,7 +33,7 @@ const Main = () => {
     return (
         <>
             <Header></Header>
-            <Container>{prod ? <Tabs Prod={prod} /> : <div>환영합니다.</div>}</Container>
+            <Container>{prod ? <Tabs Prod={prod} /> : <Loading />}</Container>
             <Footer></Footer>
         </>
     );
