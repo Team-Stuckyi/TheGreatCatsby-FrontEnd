c84ab87d8fc9b62949cfd3e2b4200eca5d9fb8d0
bmlee531@gmail.com <bmlee531@gmail.com>
2022-03-29 12:16:16 +0900

    Fix: 리뷰카드 수정
    - 별점 숫자에서 아이콘으로 변경
    - 이미지 없을 경우 noImg 표시
    - 이미지 주소 수정

--- src/components/common/ReviewCard.js
+++ src/components/common/ReviewCard.js
@@ -4,26 +4,37 @@
  * @description : 상위 컴포넌트인 ReviewList에 들어갈 항목을 정의한 스타일 컴포넌트
  */
 
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
+import NoImg from 'img/noImg.png';
+import Stars from './Stars';
+import { ServerUrl } from 'key';
 
 const ReviewContainer = styled.div`
-        display: flex;
+    display: flex;
 
-        ${(props) => props.infoContainer && css`
+    ${props =>
+        props.infoContainer &&
+        css`
             flex-direction: column;
             margin-left: 30px;
         `}
 
-        ${(props) => props.container && css`
+    ${props =>
+        props.container &&
+        css`
             padding-bottom: 20px;
             margin-top: 25px;
             border-bottom: 1px solid var(--gray300);
         `}
 
-        ${(props) => props.info && css`
+        ${props =>
+        props.info &&
+        css`
             flex-direction: row;
         `}
-        ${(props) => props.wrap && `
+        ${props =>
+        props.wrap &&
+        `
             display: inline-block;
             width: 118px;
             height: 118px;
@@ -32,8 +43,8 @@ const ReviewContainer = styled.div`
 `;
 
 const ReviewUl = styled.ul`
-            display: flex;
-            flex-direction: row;
+    display: flex;
+    flex-direction: row;
 `;
 
 const ReviewLi = styled.li`
@@ -47,34 +58,40 @@ const ReviewLi = styled.li`
     &:last-child {
         border-right: none;
     }
-`
+`;
 
 const ReviewContents = styled.p`
-    padding: 5px 0 5px 0;   
-    ${(props) => props.name && css`
-        color: var(--gray400);
-        font-size: 0.875rem;
-        line-height: 1.25rem;
-        padding-bottom: 15px;
-    `}
+    padding: 5px 0 5px 0;
+    ${props =>
+        props.name &&
+        css`
+            color: var(--gray400);
+            font-size: 0.875rem;
+            line-height: 1.25rem;
+            padding-bottom: 15px;
+        `}
 `;
 
 /**
  * @param ReviewObj 상위 컴포넌트인 Reviewlist에서 전달된 props
  */
 
-const ReviewCard = ( { ReviewObj } ) => { 
+const ReviewCard = ({ ReviewObj }) => {
     return (
         <ReviewContainer className="review-card" container={true}>
             {/* 리뷰 이미지 */}
             <ReviewContainer wrap="{true}">
-                <img src={ReviewObj.review_photo} style={{width: "118px"}}alt="리뷰이미지" />
+                {ReviewObj.review_photo ? (
+                    <img src={ServerUrl + `/imgs` + ReviewObj.review_photo} style={{ width: '118px' }} alt="리뷰이미지" />
+                ) : (
+                    <img src={NoImg} style={{ width: '118px' }} alt="리뷰이미지" />
+                )}
             </ReviewContainer>
             <ReviewContainer infoContainer={true}>
                 <ReviewContainer info={true}>
                     {/* 평점 */}
                     <div className="review-info-star">
-                        {ReviewObj.stars}
+                        <Stars starCount={ReviewObj.stars} starSize={20} />
                     </div>
                     <ReviewUl>
                         <ReviewLi>구매인증됨</ReviewLi>
@@ -83,12 +100,15 @@ const ReviewCard = ( { ReviewObj } ) => {
                     </ReviewUl>
                 </ReviewContainer>
                 {/* 작성자 */}
-                <ReviewContents name="{true}" className="anonymous"> {ReviewObj.name.substring(0,1)}*{ReviewObj.name.substring(ReviewObj.name.length-1)} </ReviewContents>
+                <ReviewContents name="{true}" className="anonymous">
+                    {' '}
+                    {ReviewObj.name.substring(0, 1)}*{ReviewObj.name.substring(ReviewObj.name.length - 1)}{' '}
+                </ReviewContents>
                 {/* 리뷰 작성글 */}
                 <ReviewContents className="text-base">{ReviewObj.review_text}</ReviewContents>
             </ReviewContainer>
         </ReviewContainer>
     );
-}
+};
 
 export default ReviewCard;
