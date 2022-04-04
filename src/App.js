70d6259a5b505c375bf6a1f84f9bea69c7dfba65
bmlee531@gmail.com <bmlee531@gmail.com>
2022-04-05 08:29:30 +0900

    Update: 관리자 페이지 로그인시 라우터 추가
    - 관리자 페이지 로그인 값 state 을 추가하여 해당 값이 true일 경우만 관리자 페이지로 진입 가능하게 변경
    - 해당 값이 없이 관리자 경로로 (/admin/*) 접속시 모두 /admin/login으로 강제 이동

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import ManageOrder from 'pages/admin/ManageOrder';
 import ManageProd from 'pages/admin/ManageProd';
 import AdminLogin from 'pages/admin/AdminLogin';
 
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import NotFound from 'pages/common/NotFound';
@@ -22,26 +23,53 @@ import NotFound from 'pages/common/NotFound';
 function App() {
     /* Login 관련 */
     const { loginSuccess, email, name, tel, user_id } = useSelector(state => state.appLogin);
+
+    /** Admin 로그인 */
+    const [isAdminLogin, setIsAdminLogin] = useState(false);
+
     return (
         <>
             <GlobalStyles />
             <Meta />
             <Routes>
+                {/* 사용자 페이지 */}
                 <Route path="/" element={<Main />} />
+                <Route path="/members/login" element={<Login />} />
                 <Route path="/product/:prod_id" element={<ProdList />} />
                 <Route path="/review/:prodId" element={<Review user_id={user_id} />} />
                 <Route path="/thankyou/:prodId" element={<ThankYou user_id={user_id} />} />
                 <Route path="/Showmethemoney/:prodId" element={<Showmethemoney user_id={user_id} name={name} email={email} />} tel={tel} />
-                <Route path="/members/login" element={<Login />} />
                 <Route path="/members/join" element={<Register />} />
                 <Route path="/regsuccess" element={<RegSuccess />} />
 
-                <Route path="/admin/manageadmin" element={<ManageAdmin />} />
-                <Route path="/admin/managemember" element={<ManageMember />} />
-                <Route path="/admin/manageprod" element={<ManageProd />} />
-                <Route path="/admin/manageorder" element={<ManageOrder />} />
-                <Route path="/admin/ManageReview" element={<ManageReview />} />
-                <Route path="/admin/login" element={<AdminLogin />} />
+                {/* 관리자 페이지 */}
+                <Route path="/admin/login" element={<AdminLogin isAdminLogin={isAdminLogin} setIsAdminLogin={setIsAdminLogin} />} />
+                {isAdminLogin ? (
+                    <>
+                        <Route
+                            path="/admin/manageadmin"
+                            element={<ManageAdmin isAdminLogin={isAdminLogin} setIsAdminLogin={setIsAdminLogin} />}
+                        />
+                        <Route
+                            path="/admin/managemember"
+                            element={<ManageMember isAdminLogin={isAdminLogin} setIsAdminLogin={setIsAdminLogin} />}
+                        />
+                        <Route
+                            path="/admin/manageprod"
+                            element={<ManageProd isAdminLogin={isAdminLogin} setIsAdminLogin={setIsAdminLogin} />}
+                        />
+                        <Route
+                            path="/admin/manageorder"
+                            element={<ManageOrder isAdminLogin={isAdminLogin} setIsAdminLogin={setIsAdminLogin} />}
+                        />
+                        <Route
+                            path="/admin/ManageReview"
+                            element={<ManageReview isAdminLogin={isAdminLogin} setIsAdminLogin={setIsAdminLogin} />}
+                        />
+                    </>
+                ) : (
+                    <Route path="/admin/*" element={<AdminLogin isAdminLogin={isAdminLogin} setIsAdminLogin={setIsAdminLogin} />} />
+                )}
 
                 <Route path="*" element={<NotFound />} />
             </Routes>
